4f44f20698759f67eee2c64a4f12db39980b6787
6bwindle <67714255+6bwindle@users.noreply.github.com>
2020-07-31 09:57:41 +1200

    Added more comments to js

--- accounts.js
+++ accounts.js
@@ -1,5 +1,5 @@
-delete_id = null
-change_pwd_id = null
+delete_id = null //id of the account that should be deleted
+change_pwd_id = null //id of the account that should change it's password
 document.getElementById("confirm-delete-container").addEventListener("click", function(e){
     e.stopPropagation()
 })
@@ -12,14 +12,16 @@ document.getElementById("change-password-container").addEventListener("click", f
     e.stopPropagation()
 })
 
-document.getElementById("add-user").addEventListener("click", function(){
+//All of the stop propogation calls make sure that clicking on the dialogue (and not the background) does not close the dialogue
+
+document.getElementById("add-user").addEventListener("click", function(){ //this show the relevant popup when the add user button is clicked
     document.getElementById("form-background").style.display = "block";
     $("#form-background").children().hide()
     $("#add-user-container").show()
     $("html").css("overflow", "hidden")
 })
 
-document.getElementById("form-background").addEventListener("click", function(){
+document.getElementById("form-background").addEventListener("click", function(){ //this closes all of the dialogues when the background is clicked
     document.getElementById("form-background").style.display = "none"
     $("#acc-pwd").val("")
     $("#acc-con-pwd").val("")
@@ -33,13 +35,13 @@ document.getElementById("form-background").addEventListener("click", function(){
     $("#con-change-pwd-input").val('')
 })
 
-function removeErrorlabel(){
+function removeErrorlabel(){ //hides the error label from the forms
     $("#error-label").fadeOut();
     setTimeout(() =>{$("#error-label").remove()}, 400)
 }
 
 
-document.getElementById("no").addEventListener("click", function(){
+document.getElementById("no").addEventListener("click", function(){ //hides the dialogue when the user decides not to delete a user
     
     $("#form-background").hide()
     $("html").css("overflow",  "scroll")
@@ -48,38 +50,38 @@ document.getElementById("no").addEventListener("click", function(){
 
 
 $(function(){
-    $("#add-user-form").submit(function(e){
-        e.preventDefault();
-        var password1 = $("#acc-pwd").val()
-        var password2 = $("#acc-con-pwd").val()
-        var username = $("#acc-uname").val().trim()
-        var admin_rights = (document.getElementById("admin-input").checked ? 1 : 0)
+    $("#add-user-form").submit(function(e){ //this triggers when the form is submitted
+        e.preventDefault(); //prevents the form from changing the page when submitted
+        var password1 = $("#acc-pwd").val() //gets the value of the first password input
+        var password2 = $("#acc-con-pwd").val() // ""              "" second password input                 
+        var username = $("#acc-uname").val().trim() //gets the username and trims it
+        var admin_rights = (document.getElementById("admin-input").checked ? 1 : 0) //a ternary operator that asigns admin rights based on whether or not the checkboc has been checked
         
-        if (username == "" || password1 == ""){
+        if (username == "" || password1 == ""){ //if either the username or the first password have not been entered
             $("#add-user-form").append("<div id='error-label'><p>Error: Please enter a username and password</p></div>")
             setTimeout(() =>{removeErrorlabel()}, 1000)
             return
         }
-        if (password1 != password2){
+        if (password1 != password2){ //if password one and two dont match (which means this also catches if the second password is null)
             $("#add-user-form").append("<div id='error-label'><p>Error: Passwords do not match</p></div>")
             setTimeout(() =>{removeErrorlabel()}, 1000)
             return
         }
 
-        $("#user-loader").show()
-        $("#error-label").remove()
-        $.ajax({
+        $("#user-loader").show() //shows the loading icon
+        $("#error-label").remove() //deletes the error label, incase an error occurs so that there are not 2 errors on the form
+        $.ajax({ //just an ajax call to the relevant php file
             type: "POST",
             url: "alter_users.php",
             data : {uname : username, pwd: password1, admin: admin_rights, type: 0},
             success: function(data){
-                if (data == ""){
+                if (data == ""){ //if there were no errors reload the page so that the changes can be seen by the user
                     window.location.reload()
                 }
                 else{
-                    $("#user-loader").hide()
+                    $("#user-loader").hide() //hides the loader and creates the error label
                     $("#add-user-form").append("<div id='error-label'><p>" +data + "</p></div>")
-            setTimeout(() =>{removeErrorlabel()}, 1000)
+            setTimeout(() =>{removeErrorlabel()}, 1000) //deletes the error label after a set amount of time
                 }
             }
         })
@@ -87,7 +89,7 @@ $(function(){
 })
 
 
-$(document).on("click", ".delete-button", function(){
+$(document).on("click", ".delete-button", function(){ //this just creates a popup asking if the user is sure they want to delete a user
    delete_id = $(this).data("id")
    $("#form-background").children().hide()
    $("html").css("overflow", "hidden")
@@ -98,12 +100,12 @@ $(document).on("click", ".delete-button", function(){
 
 })
 
-function hideBigError(){
+function hideBigError(){ //function for hiding the big error label
     $("#big-error").fadeOut();
     setTimeout(() =>{$("#big-error").hide()}, 400)
 }
 
-document.getElementById("yes").addEventListener("click", function(){
+document.getElementById("yes").addEventListener("click", function(){ //this function just calls the delete function of the php page, and if there is an error, shows the error popup
     $("#delete-loader").show()
     if (delete_id != null){
         
@@ -125,7 +127,7 @@ document.getElementById("yes").addEventListener("click", function(){
     })}
 })
 
-$(document).on("click", ".change-button", function(){
+$(document).on("click", ".change-button", function(){ //shows the relevant form
     change_pwd_id = $(this).data("id")
     $("#change-password-form h2").html("Enter a new password for " + $(this).data("name"))
     $("#form-background").children().hide()
@@ -137,16 +139,16 @@ $(document).on("click", ".change-button", function(){
 $(function(){
     $("#change-password-form").submit(function(e){
         
-        e.preventDefault();
+        e.preventDefault(); //stops the page from changing
         $("#error-label").remove()
-        if (change_pwd_id == null){
+        if (change_pwd_id == null){ //if the form is somehow shown without an id being set (possible if the user shows it using inspect elemt)
             $("#change-password-form").append("<div id='error-label'><p> Error: What have you done? </p></div>")
             setTimeout(() =>{removeErrorlabel()}, 1000)
         }
         else{
 
         
-        var password1 = $("#change-pwd-input").val()
+        var password1 = $("#change-pwd-input").val() //the if states below are the same as the add user, just checking for null and non matching passwords
         var password2 = $("#con-change-pwd-input").val()
         if(password1 == ""){
             $("#change-password-form").append("<div id='error-label'><p> Error: Please enter a password </p></div>")
@@ -157,8 +159,8 @@ $(function(){
             setTimeout(() =>{removeErrorlabel()}, 1000)
         }
         else{
-            $("#change-loader").show()
-            $.ajax({
+            $("#change-loader").show() //shows the loader
+            $.ajax({ //ajax call to php, same as others
                 type:"POST",
                 url: "alter_users.php",
                 data: {id: change_pwd_id, pwd : password1, type:2},
@@ -167,10 +169,10 @@ $(function(){
                         
                         $("#change-password-form").append("<p id='error-label'> " + data + " </p>")
                         setTimeout(() =>{removeErrorlabel()}, 1000)
-                        $("#change-loader").hide()
+                        $("#change-loader").hide() //shows error, same as others
                     }
                     else{
-                        window.location.reload()
+                        window.location.reload() //reloads the page, same as the other ajax calls
                     }
                 }
             })
